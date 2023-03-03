2e4929e19051783e5f858683f948a08a0e11d9bf
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-02 20:46:34 -0500

    Installed and used express.js as a middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http=require("http")
-const fs=require("fs")
-const route=require("./routes")//not global so must give path
-const server = http.createServer( route ) //req and res are objects
-    // console.log(req["url"],req["method"],req.headers)
-    // console.log("\n"+res)
- //createserver has a function as a parameter which will be executed every time a request has been made to this server
-// hence, we can write this function and pass it as arguement. hover over the createserver method to see the details
 
-server.listen(8000) //in production it will take the port number:80
-// the above method keeps listening for requests
-// console.log("hey,, this line written to see if nodemon works")
+const express=require("express") //clearly define the imports; all core modules in one palce, third party modules in another 
+
+const app=express()
+
+app.use((req,res,next) => { //passing a function to use where next is also a function
+    console.log("Inside a middleware")
+    next(); //use next in order for the next middleware functions to be used else this will be the last middle ware functions
+})
+
+app.use((req,res,next)=>{
+    console.log("inside another middleware")
+})
+
+const server=http.createServer(app)
+
+server.listen(8000)
