4d589febc449756a0e7ec3cc7b43eebd4f24130c
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-03 14:06:18 -0500

    further styling and storing products

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const path=require("path")
 
+const products=require("./admin").products
+
 router.get("/users",(req,res,next) => {
     console.log("inside the users middle function")
     res.send("<h2> Hi user!!!</h2>")
@@ -12,6 +14,7 @@ router.get("/users",(req,res,next) => {
 router.use('/shop',(req,res,next) => { //passing a function to use where next is also a function
     console.log("Inside a middleware")
     res.sendFile(path.join(__dirname,"..","views","shop.html"))
+    console.log("shop",products)
     // next(); //use next in order for the next middleware functions to be used else this will be the last middle ware functions
 })
 
