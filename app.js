90245429733c0a9d147702e554b33b36bb2210f4
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-02 21:18:57 -0500

    mapping the routes

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express=require("express") //clearly define the imports; all core modules 
 
 const app=express()
 
-app.use((req,res,next) => { //passing a function to use where next is also a function
-    console.log("Inside a middleware")
-    next(); //use next in order for the next middleware functions to be used else this will be the last middle ware functions
+// map the routes so that all can be reachable if .use("/") is in the top then other functions will be never be reachable and also donot use next().
+// we dont want to send 2 response objects 
+
+app.use("/users",(req,res,next) => {
+    console.log("inside the users middle function")
+    res.send("<h2> Hi user!!!</h2>")
 })
 
-app.use((req,res,next)=>{
-    console.log("inside another middleware")
-    res.send("<h2>Welcome to express.js!")
+app.use('/',(req,res,next) => { //passing a function to use where next is also a function
+    console.log("Inside a middleware")
+    res.send("<h2>Welcome to express.js!, this is the default webpage</h2>")
+    // next(); //use next in order for the next middleware functions to be used else this will be the last middle ware functions
 })
 
 app.listen(8000) //helps write clean code.
