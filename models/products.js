2c0c66a27578f8834b427a6a6f62ef351e5d60d0
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-07 08:47:47 -0500

    Refactoring(using elegant js callbacks)

--- models/products.js
+++ models/products.js
@@ -1,49 +1,39 @@
 const fs = require('fs');
 const path = require('path'); //
 // undoing a commit. and then pushing a new commit to remote after rebasing
-
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+const getProductsFromFile = cb => {fs.readFile(p, (err, fileContent) => {
+  if (err) {
+    // cb([]); no file content is not an error
+    console.log(err);
+  }
+  console.log(fileContent);
+  if(fileContent.length>0){
+    cb(JSON.parse(fileContent)); //after running the callback the remaining function is returned???--no // basically throwing an error previoulsy cuz of json.parse(null)
+    return
+  }
+  cb([]);
+});}
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) { 
-        if (fileContent.length>0) //reading an empty json file is not giving an error as expected so need to catch when fileLength==0 differently
-        products = JSON.parse(fileContent);
-      }
-      console.log(this);
+    getProductsFromFile(products => {
+      // console.log(this); brilliant execution
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
-    });
+    })
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        // cb([]); no file content is not an error
-        console.log(err);
-      }
-      console.log(fileContent);
-      if(fileContent.length>0){
-        cb(JSON.parse(fileContent)); //after running the callback the remaining function is returned???--no // basically throwing an error previoulsy cuz of json.parse(null)
-        return
-      }
-      cb([]);
-    });
+    getProductsFromFile(cb)
   }
 };
