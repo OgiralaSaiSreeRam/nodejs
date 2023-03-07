ca55e536d30eada2ba802d82979693d3ce242d2f
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-03-07 07:40:37 -0500

    Adding model to the project(Very important)

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,26 +1,30 @@
+const Product = require('../models/products');
 
-const products=[]
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
-exports.getAddProduct = (req,res,next)=>{ //get uses exact matching unlike use. now this will not be executed for /form/fhfhf
-    // res.sendFile(path.join(__dirname,"..","views","add-product.html"))
-                    // can also use action="form"
-        res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/shop');
+};
 
-}
-
-
-exports.postAddProduct= (req,res,next)=>{ //this webpage is called from the /form in a post method but can also call it with get using
-    
-    products.push({ title: req.body.title });
-    console.log("admin",products)               //but i want to access this webpage only using a post method then use app.post()
-    res.redirect('/shop')
-}
-
-exports.getProducts = (req,res,next) => { //passing a function to use where next is also a function
-    console.log("Inside a middleware")
-    // res.sendFile(path.join(__dirname,"..","views","shop.html"))
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
-    //need to send js objects to the render method do that the pug file/template engine can load it
-    console.log("shop",products)
-    // next(); //use next in order for the next middleware functions to be used else this will be the last middle ware functions
-}
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
---
+++ models/products.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) { 
+        if (fileContent.length>0) //reading an empty json file is not giving an error as expected so need to catch when fileLength==0 differently
+        products = JSON.parse(fileContent);
+      }
+      console.log(this);
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        // cb([]); no file content is not an error
+        console.log(err);
+      }
+      console.log(fileContent);
+      if(fileContent.length>0){
+        cb(JSON.parse(fileContent)); //after running the callback the remaining function is returned???--no // basically throwing an error previoulsy cuz of json.parse(null)
+        return
+      }
+      cb([]);
+    });
+  }
+};
