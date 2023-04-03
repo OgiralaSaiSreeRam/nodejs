2d8afa5f820ff5d8601212c541985c00aef7c15a
Sreeram Ogirala <sreeramogirala@gmail.com>
2023-04-03 16:51:14 -0400

    testing on the react api

--- test/start.js
+++ test/start.js
@@ -1,9 +1,10 @@
 const expect = require('chai').expect;
-
-it('should add numbers correctly', function() {
+// it is a fucntion that is used to write the error message. First arg is testcase name, 2nd is the callback
+//can write what should happen inside the function
+it('should add numbers correctly', function() { 
     const num1 = 2;
     const num2 = 3;
-    expect(num1 + num2).to.equal(5);
+    expect(num1 + num2).to.equal(5); //expect is just to check if the condition is satisfied
 })
 
 it('should not give a result of 6', function() {
